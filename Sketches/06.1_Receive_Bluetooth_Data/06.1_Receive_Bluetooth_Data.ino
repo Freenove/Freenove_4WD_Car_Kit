fb9a303a857d6f1a8a931b3b27326d6ad36cff1c
Zhentao-Lin <173039613@qq.com>
2024-10-24 14:00:52 +0800

    Update 06.1_Receive_Bluetooth_Data.ino

--- Sketches/06.1_Receive_Bluetooth_Data/06.1_Receive_Bluetooth_Data.ino
+++ Sketches/06.1_Receive_Bluetooth_Data/06.1_Receive_Bluetooth_Data.ino
@@ -1,52 +1,54 @@
-/**********************************************************************
-  Filename    : Receive_Bluetooth_Data.ino
-  Product     : Freenove 4WD Car for UNO
-  Description : Receive data from bluetooth and print it to monitor.
-  Auther      : www.freenove.com
-  Modification: 2022/7/11
-  Notes       : This code comes from the sample program SerialEvent.ino.
-**********************************************************************/
-String inputString = "";         // a String to hold incoming data
-bool stringComplete = false;  // whether the string is complete
-
-void setup() {
-  // initialize serial:
-  Serial.begin(9600);
-  Serial.println("AT+NAMEBT05-GEN");  // Or use Serial.print("AT+NAMEBT05-GEN\r\n");
-  delay(200);
-  Serial.println("AT+NAMBBT05-BLE");  // Or use Serial.print("AT+NAMEBT05-BLE\r\n");
-  delay(200);
-  Serial.println("AT+ROLE0");
-  delay(200);
-  // reserve 200 bytes for the inputString:
-  inputString.reserve(200);
-}
-
-void loop() {
-  // print the string when a newline arrives:
-  if (stringComplete) {
-    Serial.println(inputString);
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
-  }
-}
-
-/*
-  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
-  routine is run between each time loop() runs, so using delay inside loop can
-  delay response. Multiple bytes of data may be available.
-*/
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, set a flag so the main loop can
-    // do something about it:
-    if (inChar == '\n') {
-      stringComplete = true;
-    }
-  }
+String inputString = "";         // a String to hold incoming data  
+bool stringComplete = false;     // whether the string is complete  
+long baudRate[] = {2400, 9600, 19200, 115200};  
+int baudRateCount = sizeof(baudRate) / sizeof(baudRate[0]);  
+  
+void setup() {  
+  // Loop through each baud rate and send commands  
+  for (int i = 0; i < baudRateCount; i++) {  
+    Serial.begin(baudRate[i]);  
+    delay(100);  
+    Serial.flush();
+    Serial.println("AT+NAME=BT05");  // Or use Serial.print("AT+NAMEBT05-BLE\r\n");  
+    delay(200);  
+    Serial.println("AT+ROLE=0");  
+    delay(200);  
+    Serial.println("AT+UART=4");  
+    delay(200);  
+  }  
+  
+  // Finally, set the serial to the desired baud rate (e.g., 115200)  
+  Serial.begin(115200);  
+    
+  // reserve 200 bytes for the inputString:  
+  inputString.reserve(200);  
+}  
+  
+void loop() {  
+  // print the string when a newline arrives:  
+  if (stringComplete) {  
+    Serial.println(inputString);  
+    // clear the string:  
+    inputString = "";  
+    stringComplete = false;  
+  }  
+}  
+  
+/*  
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This  
+  routine is run between each time loop() runs, so using delay inside loop can  
+  delay response. Multiple bytes of data may be available.  
+*/  
+void serialEvent() {  
+  while (Serial.available()) {  
+    // get the new byte:  
+    char inChar = (char)Serial.read();  
+    // add it to the inputString:  
+    inputString += inChar;  
+    // if the incoming character is a newline, set a flag so the main loop can  
+    // do something about it:  
+    if (inChar == '\n') {  
+      stringComplete = true;  
+    }  
+  }  
 }
